90515cfe37ac8c1f319e398dc0a9ab5fc0dfdaf5
Max <mossi_wood@yahoo.com>
2024-09-15 01:25:34 -0700

    Edit branding

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -48,17 +48,17 @@ export const Badge = (props: Props) => {
             'background-color': props.badgeBackgroundColor ?? '#ffffff',
           }}
         >
-          {props.footer?.text ?? 'Powered by'}
+          {props.footer?.text ?? 'AI powered support. Need more help?'}
           <a
             ref={liteBadge}
-            href={props.footer?.companyLink ?? 'https://flowiseai.com'}
+            href={props.footer?.companyLink ?? 'https://cross-checks.com/contact/'}
             target="_blank"
             rel="noopener noreferrer"
             class="lite-badge"
             id="lite-badge"
             style={{ 'font-weight': 'bold', color: props.footer?.textColor ?? props.poweredByTextColor ?? defaultTextColor }}
           >
-            <span>&nbsp;{props.footer?.company ?? 'Flowise'}</span>
+            <span>&nbsp;{props.footer?.company ?? 'Contact us'}</span>
           </a>
         </span>
       </Show>
